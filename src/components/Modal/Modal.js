0699970756a5285554f83b754a5a2be666161381
andrewbal93 <131342609+andrewbal93@users.noreply.github.com>
2024-01-07 23:40:43 +0200

    Modal fix

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,9 +5,12 @@ export default function Modal({ selectedHit, hideModal }) {
   const handleEscClick = e => e.code === 'Escape' && hideModal();
 
   useEffect(() => {
+    const handleEscClick = e => e.code === 'Escape' && hideModal();
+
     document.addEventListener('keydown', handleEscClick);
+
     return () => document.removeEventListener('keydown', handleEscClick);
-  }, []);
+  }, [hideModal]);
 
   return (
     <div className={styles['Overlay']}>
